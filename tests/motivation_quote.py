dc76621d7268dd41968e1e1d6a9ba760deec5f77
PaladinsCode <78800426+PaladinsCode@users.noreply.github.com>
2023-09-24 16:00:19 +0300

    Modifies general file structures to exclde necessary files

--- tests/motivation_quote.py
+++ tests/motivation_quote.py
@@ -1,34 +1,56 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import random
+
+# # Define the URL of the website you want to scrape
+# url = "https://www.brainyquote.com/quote_of_the_day"
+
+# # Set a User-Agent header to mimic a real web browser
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+# }
+
+# try:
+#     # Send an HTTP GET request to the URL with headers
+#     response = requests.get(url, headers=headers)
+
+#     # Check the response status code
+#     if response.status_code == 200:
+#         # Parse the HTML content of the page
+#         soup = BeautifulSoup(response.text, 'html.parser')
+
+#         # Find the element containing the motivational quote
+#         quote_element = soup.find("a", {"title": "view quote"})
+
+#         # Extract the text of the quote
+#         quote = quote_element.get_text()
+
+#         # Print the motivational quote
+#         print("Motivational Quote of the Day:")
+#         print(quote)
+#     else:
+#         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+# except requests.exceptions.RequestException as e:
+#     print(f"An error occurred: {e}")
+
+
 import requests
-from bs4 import BeautifulSoup
-import random
 
-# Define the URL of the website you want to scrape
-url = "https://www.brainyquote.com/quote_of_the_day"
-
-# Set a User-Agent header to mimic a real web browser
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-}
+# Define the URL
+url = "https://type.fit/api/quotes"
 
 try:
-    # Send an HTTP GET request to the URL with headers
-    response = requests.get(url, headers=headers)
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
 
-    # Check the response status code
+    # Check if the response status code is 200 (OK)
     if response.status_code == 200:
-        # Parse the HTML content of the page
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # Parse the JSON response
+        data = response.json()
 
-        # Find the element containing the motivational quote
-        quote_element = soup.find("a", {"title": "view quote"})
-
-        # Extract the text of the quote
-        quote = quote_element.get_text()
-
-        # Print the motivational quote
-        print("Motivational Quote of the Day:")
-        print(quote)
+        # Print the data
+        print(data)
     else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
